db889f28bd4b18b21d81c407f96cbddd7f40d00e
Vinh Nguyen <themesexperts1986@gmail.com>
2020-04-08 13:20:26 +0700

    finish challenge 9

--- app.js
+++ app.js
@@ -12,18 +12,40 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.use(express.static("public"));
 
+app.get('/', function(req, res) {
 
+  res.render('home', {
+    startingContent: homeStartingContent
+  });
 
+});
 
+app.post('/compose', function(req, res) {
+  // console.log("Hello world");
+  console.log(req.body.postTitle);
+});
 
+app.get('/about', function(req, res) {
+  res.render('about', {
+    aboutContent: aboutContent
+  });
+});
 
+app.get('/contact', function(req, res) {
+  res.render('contact', {
+    contactContent: contactContent
+  });
+});
 
-
-
-
+app.get('/compose', function(req, res) {
+  res.render('compose');
+});
 
 
 app.listen(3000, function() {
