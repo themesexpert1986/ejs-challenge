7edd59abc23d7bc5d0f4cb2e48f9c0df227ccaba
Vinh Nguyen <themesexperts1986@gmail.com>
2020-04-08 13:58:56 +0700

    update Challenge 11 - Global Variable

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+var posts = [];
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get('/', function(req, res) {
-
+  console.log(posts);
   res.render('home', {
     startingContent: homeStartingContent
   });
@@ -32,7 +32,7 @@ app.post('/compose', function(req, res) {
     'content': req.body.postBody
   }
 
-  console.log(post);
+  posts.push(post);
   res.redirect('/');
 
 });
